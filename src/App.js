33f1583b062455d0fb3c1e6c01b5a006aba98eb7
M-Nyaboke <miriamnyaboke816@gmail.com>
2024-07-24 11:54:35 +0300

    image gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Typography, Input, List, Card, Image, Space } from "antd";
 
 function App() {
+
+  const [ searchedText, setSearchedText ] = useState("");
+  const [ dataSource, setDataSource ] = useState([]);
+  const [ loading, setLoading ] = useState(false);
+  const [ previewImages, setPreviewImages ] = useState([]);
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`https://dummyjson.com/products/search?q=${searchedText}`)
+    .then(res => res.json())
+    .then(response => {
+      setLoading(false)
+      setDataSource(response.products)
+    })
+  }, [searchedText])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Space style={{ padding: "0px 16px" }} direction='vertical'>
+      <Typography.Title style={{ textAlign: "center", fontFamily: "monospace" }}>
+        Miriam's Gallery
+      </Typography.Title>
+      <Input.Search 
+        style={{ maxWidth: 500, display: "flex", margin: "auto" }}
+          onSearch={(value)=>{
+            setSearchedText(value)
+          }}
+        
+      >
+      </Input.Search>
+      <Typography.Text>Showing results for: <Typography.Text strong>{searchedText||"All"}</Typography.Text></Typography.Text>
+      <List 
+        loading={loading} 
+        dataSource={dataSource} 
+        grid={{xs:1,sm: 2,md:3,lg:4,xl:5,xxl:6}}
+        renderItem={(item) => {
+          return <Card hoverable key={item.id} style={{ height: 300, margin: 12, overflow:"hidden" }} >
+            <Image 
+              src={item.thumbnail} 
+              preview={{ visible: false }}
+              onClick={() => {
+                setPreviewImages(item.images)
+              }} 
+            >              
+            </Image>
+          </Card>
+      }}></List>
+      {
+        previewImages.length> 0 ? ( 
+          <Image.PreviewGroup 
+            preview={{ 
+              visible: previewImages.length, 
+              onVisibleChange:(value)=>{
+                if (!value) {
+                  setPreviewImages([]);
+                }
+              }
+            }}
+          >
+          {previewImages.map((image) => {
+            return <Image src={image} />
+          })}
+          </Image.PreviewGroup>
+        ) : null }
+      
+    </Space>
   );
 }
 
